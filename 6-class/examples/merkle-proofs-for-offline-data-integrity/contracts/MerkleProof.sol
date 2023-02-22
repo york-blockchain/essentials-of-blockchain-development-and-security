a6961ba8d0053817da2ef45dd4cc1623b7285c15
dhruvinparikh <imsilent93@gmail.com>
2023-02-22 09:04:04 -0500

    init class7

---
+++ 6-class/examples/merkle-proofs-for-offline-data-integrity/contracts/MerkleProof.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: Public Domain
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract MerkleProof {
+    uint merkleRoot;
+
+    function getRoot() public view returns (uint) {
+      return merkleRoot;
+    }
+
+    // Extremely insecure, in production code access to
+    // this function MUST BE strictly limited, probably to an
+    // owner
+    function setRoot(uint _merkleRoot) external {
+      merkleRoot = _merkleRoot;
+    }   // setRoot
+
+    
+    // Same functions as in the JavaScript, just written in Solidity
+    // (and no strings needed)
+    function hash(uint _a) internal pure returns(uint) {
+      return uint(keccak256(abi.encode(_a)));
+    }
+
+    function pairHash(uint _a, uint _b) internal pure returns(uint) {
+      return hash(hash(_a) ^ hash(_b));
+    }
+
+    // Verify a Merkle proof
+    function verifyProof(uint _value, uint[] calldata _proof) 
+        public view returns (bool) {
+      uint temp = _value;
+      uint i;
+  
+      for(i=0; i<_proof.length; i++) {
+        temp = pairHash(temp, _proof[i]);
+      }
+
+      return temp == merkleRoot;
+    }
+    
+}  // MerkleProof
